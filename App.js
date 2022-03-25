8f5296c4fa61de9fc007a409681075ab9b239f1e
Davi Oliveira <davidomingos816@gmail.com>
2022-03-25 01:45:45 -0300

    Commit inicial

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Title from './src/components/Title/'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Title/>
     </View>
   );
 }
---
+++ src/components/Title/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native"
+
+export default function Title(){
+    return(
+        <View>
+            <Text>OneBitHealth</Text>
+        </View>
+    );
+
+
+}
